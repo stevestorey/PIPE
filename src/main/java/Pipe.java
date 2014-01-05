38e41d62a54b1f9fe07128562612e4a319645352
Steve Storey <sstorey@gmail.com>
2014-01-05 16:25:01 +0000

    Tidy up the Pipe executor class.

--- src/main/java/Pipe.java
+++ src/main/java/Pipe.java
@@ -5,39 +5,32 @@ import pipe.views.PipeApplicationView;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
+
 public class Pipe
 {
-
-	private PipeApplicationModel applicationModel;
-	@SuppressWarnings("unused")
-	private PipeApplicationController applicationController;
-
-    private PipeApplicationView applicationView;
 
 	private Pipe(String version)
     {
 
         CopyPasteManager copyPaste = new CopyPasteManager();
-        applicationModel = new PipeApplicationModel(version);
-        applicationController = new PipeApplicationController(copyPaste, applicationModel);
-        applicationView = new PipeApplicationView(applicationController, applicationModel);
+        PipeApplicationModel applicationModel = new PipeApplicationModel(version);
+        PipeApplicationController applicationController = new PipeApplicationController(copyPaste, applicationModel);
+        new PipeApplicationView(applicationController, applicationModel);
     }
     public static void main(String args[])
     {
         Runnable runnable = pipeRunnable();
         SwingUtilities.invokeLater(runnable);
     }
+
 	protected static Runnable pipeRunnable()
 	{
-		Runnable runnable = new Runnable()
-                            {
-                                public void run()
-                                {
-                                    @SuppressWarnings("unused")
-									Pipe pipe = new Pipe("v4.3.0");
-                                }
-                            };
-		return runnable;
+		return new Runnable() {
+            public void run()
+            {
+				new Pipe("v4.3.0");
+            }
+        };
 	}
     protected static void runPipeForTesting() throws InterruptedException, InvocationTargetException
     {
